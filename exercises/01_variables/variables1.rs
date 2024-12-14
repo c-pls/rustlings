9f5afc8b280a3bb3acab01b1553ead9db2570e2c
c-pls <iamdchinh@gmail.com>
2024-12-14 15:44:33 +0700

    Add variable exercises and solutions with corrections

--- exercises/01_variables/variables1.rs
+++ exercises/01_variables/variables1.rs
@@ -1,6 +1,12 @@
 fn main() {
     // TODO: Add the missing keyword.
-    x = 5;
+    let x: i32 = 5;
 
     println!("x has the value {x}");
+
+    // shadowing
+    let x = x * x;
+
+    println!("{x}");
+
 }
