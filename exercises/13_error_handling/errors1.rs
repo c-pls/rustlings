c5524d23b2eb1eb42a72aba38c09e38dbb31fdd7
c-pls <iamdchinh@gmail.com>
2024-12-23 23:28:38 +0700

    Refactor error handling exercises to improve clarity and functionality; update function signatures and error propagation

--- exercises/13_error_handling/errors1.rs
+++ exercises/13_error_handling/errors1.rs
@@ -4,13 +4,48 @@
 // construct to `Option` that can be used to express error conditions. Change
 // the function signature and body to return `Result<String, String>` instead
 // of `Option<String>`.
-fn generate_nametag_text(name: String) -> Option<String> {
+fn generate_nametag_text(name: String) -> Result<String, String> {
     if name.is_empty() {
         // Empty names aren't allowed
-        None
+        Err(String::from("Empty names aren't allowed"))
     } else {
-        Some(format!("Hi! My name is {name}"))
+        Ok(format!("Hi! My name is {name}"))
     }
+}
+
+use std::fs::File;
+use std::io::{self, Read};
+fn read_username_from_file() -> Result<String, io::Error> {
+    let username_file_result = File::open("hello.txt");
+
+    let mut username_file = match username_file_result {
+        Ok(file) => file,
+        Err(e) => return Err(e),
+    };
+
+    let mut username = String::new();
+
+    match username_file.read_to_string(&mut username) {
+        Ok(_) => Ok(username),
+        Err(e) => Err(e),
+    }
+}
+
+fn shorter_way() -> Result<String, io::Error> {
+    let mut usename_file_result = File::open("hello.txt")?;
+
+    let mut username = String::new();
+
+    usename_file_result.read_to_string(&mut username)?;
+
+    Ok(username)
+
+    // fs::read_to_string("hello.txt")
+}
+
+// ? operator works with Option
+fn last_char_of_first_line(text: &str) -> Option<char> {
+    text.lines().next()?.chars().last()
 }
 
 fn main() {
