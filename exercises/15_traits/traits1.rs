81c9cc19a952e087eebedce202fb664dccacf9af
c-pls <iamdchinh@gmail.com>
2024-12-24 20:24:42 +0700

    Implement traits exercises with new methods for String and Vec; add default behavior for licensing info

--- exercises/15_traits/traits1.rs
+++ exercises/15_traits/traits1.rs
@@ -6,12 +6,16 @@ trait AppendBar {
 
 impl AppendBar for String {
     // TODO: Implement `AppendBar` for the type `String`.
+    fn append_bar(self) -> Self {
+        format!("{}Bar",self)
+    }
 }
 
 fn main() {
     let s = String::from("Foo");
     let s = s.append_bar();
     println!("s: {s}");
+
 }
 
 #[cfg(test)]
